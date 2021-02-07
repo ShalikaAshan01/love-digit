3f26d6b901b75ff7f521cfc6d1d163fef62c6220
ShalikaAshan01 <shalikaashan@outlook.com>
2021-02-07 14:56:13 +0530

    tap back button to close the app

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -16,155 +16,150 @@ class HomeView extends StatelessWidget {
     final imageWidth = imageHeight * aspectRatio;
     final duration = Duration(seconds: 1);
 
-    return WillPopScope(
-      onWillPop: () {
-        return Future.value(false);
-      },
-      child: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(MaterialCommunityIcons.account_outline),
-              label: 'Profile',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(MaterialCommunityIcons.account_plus_outline),
-              label: 'Add a Partner',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(MaterialCommunityIcons.star_outline),
-              label: 'Celebrity',
-            ),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                width: imageWidth,
-                height: imageHeight,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        PageRouteBuilder(
-                          transitionDuration: duration,
-                          reverseTransitionDuration: duration,
-                          pageBuilder:
-                              (context, animation, secondaryAnimation) {
-                            final curveAnimation = CurvedAnimation(
-                                parent: animation, curve: Interval(0, 0.5));
-                            return FadeTransition(
-                                opacity: curveAnimation,
-                                child: CelebrityView(
-                                  duration: duration,
-                                ));
-                          },
-                        ));
-                  },
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Hero(
-                          tag: celebrity.id,
-                          flightShuttleBuilder: (flightContext, animation, flightDirection, fromHeroContext, toHeroContext) {
-                            return ClipRRect(
-                                borderRadius:
-                                BorderRadius.circular(circularBorderRadius),
-                                child: Image.asset(
-                                  celebrity.image,
-                                  fit: BoxFit.cover,
-                                ));
-                          },
-                          child: SizedBox.expand(
-                              child: Material(
-                            elevation: 10,
-                            type: MaterialType.transparency,
-                            borderRadius:
-                                BorderRadius.circular(circularBorderRadius),
-                            child: ClipRRect(
-                                borderRadius:
-                                    BorderRadius.circular(circularBorderRadius),
-                                child: Image.asset(
-                                  celebrity.image,
-                                  fit: BoxFit.cover,
-                                )),
-                          ))),
-                      Positioned(
-                        left: SizeConfig.blockSizeHorizontal * 3,
-                        top: SizeConfig.blockSizeHorizontal * 4,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Hero(
-                                tag: "name-${celebrity.id}",
-                                flightShuttleBuilder: (flightContext, animation, flightDirection, fromHeroContext, toHeroContext) {
-                                  return Text(
-                                    celebrity.name,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline5
-                                        .copyWith(
-                                        fontSize: 18, color: Colors.white),
-                                  );
-                                },
-                                child: Text(
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(MaterialCommunityIcons.account_outline),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MaterialCommunityIcons.account_plus_outline),
+            label: 'Add a Partner',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(MaterialCommunityIcons.star_outline),
+            label: 'Celebrity',
+          ),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: imageWidth,
+              height: imageHeight,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        transitionDuration: duration,
+                        reverseTransitionDuration: duration,
+                        pageBuilder:
+                            (context, animation, secondaryAnimation) {
+                          final curveAnimation = CurvedAnimation(
+                              parent: animation, curve: Interval(0, 0.5));
+                          return FadeTransition(
+                              opacity: curveAnimation,
+                              child: CelebrityView(
+                                duration: duration,
+                              ));
+                        },
+                      ));
+                },
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Hero(
+                        tag: celebrity.id,
+                        flightShuttleBuilder: (flightContext, animation, flightDirection, fromHeroContext, toHeroContext) {
+                          return ClipRRect(
+                              borderRadius:
+                              BorderRadius.circular(circularBorderRadius),
+                              child: Image.asset(
+                                celebrity.image,
+                                fit: BoxFit.cover,
+                              ));
+                        },
+                        child: SizedBox.expand(
+                            child: Material(
+                          elevation: 10,
+                          type: MaterialType.transparency,
+                          borderRadius:
+                              BorderRadius.circular(circularBorderRadius),
+                          child: ClipRRect(
+                              borderRadius:
+                                  BorderRadius.circular(circularBorderRadius),
+                              child: Image.asset(
+                                celebrity.image,
+                                fit: BoxFit.cover,
+                              )),
+                        ))),
+                    Positioned(
+                      left: SizeConfig.blockSizeHorizontal * 3,
+                      top: SizeConfig.blockSizeHorizontal * 4,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Hero(
+                              tag: "name-${celebrity.id}",
+                              flightShuttleBuilder: (flightContext, animation, flightDirection, fromHeroContext, toHeroContext) {
+                                return Text(
                                   celebrity.name,
                                   style: Theme.of(context)
                                       .textTheme
                                       .headline5
                                       .copyWith(
-                                          fontSize: 18, color: Colors.white),
-                                )),
-                            Hero(
-                                tag: "type-${celebrity.id}",
-                                flightShuttleBuilder: (flightContext, animation, flightDirection, fromHeroContext, toHeroContext) {
-                                  return Text(
-                                    celebrity.type,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline6
-                                        .copyWith(
-                                        fontWeight: FontWeight.w300,
-                                        fontSize: 13,
-                                        color: Colors.white),
-                                  );
-                                },
-                                child: Text(
+                                      fontSize: 18, color: Colors.white),
+                                );
+                              },
+                              child: Text(
+                                celebrity.name,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline5
+                                    .copyWith(
+                                        fontSize: 18, color: Colors.white),
+                              )),
+                          Hero(
+                              tag: "type-${celebrity.id}",
+                              flightShuttleBuilder: (flightContext, animation, flightDirection, fromHeroContext, toHeroContext) {
+                                return Text(
                                   celebrity.type,
                                   style: Theme.of(context)
                                       .textTheme
                                       .headline6
                                       .copyWith(
-                                          fontWeight: FontWeight.w300,
-                                          fontSize: 13,
-                                          color: Colors.white),
-                                )),
-                          ],
-                        ),
+                                      fontWeight: FontWeight.w300,
+                                      fontSize: 13,
+                                      color: Colors.white),
+                                );
+                              },
+                              child: Text(
+                                celebrity.type,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6
+                                    .copyWith(
+                                        fontWeight: FontWeight.w300,
+                                        fontSize: 13,
+                                        color: Colors.white),
+                              )),
+                        ],
                       ),
-                      Positioned(
-                        right: SizeConfig.blockSizeHorizontal * 3,
-                        top: SizeConfig.blockSizeHorizontal * 4,
-                        child: BuildIcon(
-                          icon: MaterialCommunityIcons.dots_horizontal,
-                          onPressed: () {},
-                        ),
+                    ),
+                    Positioned(
+                      right: SizeConfig.blockSizeHorizontal * 3,
+                      top: SizeConfig.blockSizeHorizontal * 4,
+                      child: BuildIcon(
+                        icon: MaterialCommunityIcons.dots_horizontal,
+                        onPressed: () {},
                       ),
-                      Positioned(
-                        bottom: imageHeight * 0.1,
-                        child: FavourableMatch(
-                          celebrity: celebrity,
-                        ),
-                      )
-                    ],
-                  ),
+                    ),
+                    Positioned(
+                      bottom: imageHeight * 0.1,
+                      child: FavourableMatch(
+                        celebrity: celebrity,
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
